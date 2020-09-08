c5d84b4925d09f195a9a1b659699ed09b6cdc72b
Michael <mjihanda@gmail.com>
2020-09-08 11:31:06 +0700

    Updates

---
+++ precache-manifest.40b937cebbb0daa3d2b2f1e94f256b1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162b21a91814bd2cfb3d3a0a49962489",
+    "url": "/React-Website/index.html"
+  },
+  {
+    "revision": "0fb19fe651685ea48f43",
+    "url": "/React-Website/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7fae9ba1df6d7fc5d052",
+    "url": "/React-Website/static/css/main.f2ab8dd5.chunk.css"
+  },
+  {
+    "revision": "0fb19fe651685ea48f43",
+    "url": "/React-Website/static/js/2.1ed908ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Website/static/js/2.1ed908ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fae9ba1df6d7fc5d052",
+    "url": "/React-Website/static/js/main.d0b00fb9.chunk.js"
+  },
+  {
+    "revision": "149dbcba06be9b1c4c2e",
+    "url": "/React-Website/static/js/runtime-main.eab099a5.js"
+  }
+]);
